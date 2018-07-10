b3fcfef9706677cdf5f0ed623d622afe86c81b41
DESKTOP-KDNJ46P\Karthick <karthick.veeramani@gmail.com>
2018-07-10 11:07:09 +0530

    select completed

--- src/pages/fuel-entry/fuel-entry.ts
+++ src/pages/fuel-entry/fuel-entry.ts
@@ -19,10 +19,11 @@ import { FormControl } from '@angular/forms';
 })
 export class FuelEntryPage implements OnInit {
   myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+  options: string[];
   filteredOptions: Observable<string[]>;
 getVehicleDetails=[];
 vehicleNumber=[];
+getvehicleNumber;
 driverName=[];
 
 
@@ -36,47 +37,47 @@ driverName=[];
     console.log('ionViewDidLoad FuelEntryPage');
   }
   OnfuelEntry(value){
-console.log(value);
+    console.log(value);
+    console.log(value.value);
+    console.log( this.getvehicleNumber);
+    
   }
-
     ngOnInit() {
-      this.filteredOptions = this.myControl.valueChanges
-        .pipe(
-          startWith(''),
-          map(value => this._filter(value))
-        );
+      
   }
 
   private _filter(value: string): string[] {
+    this.getvehicleNumber=value;
     const filterValue = value.toLowerCase();
+    let result= this.getVehicleDetails.filter(proj => proj.RegNo === value);
+    result.forEach(data =>{
+      this.driverName.push(data["DriverName"]);
+
+    })
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
-  getDriver(){
-    this.driverName = [];
-
-   let result= this.getVehicleDetails.filter(proj => proj.RegNo === "TN21AX4273");
-   result.forEach(data =>{
-    console.log(data["DriverName"]);
-   })
-  
-  }
   
 
   getVDetails() {
+  //  this.options=[];
     this.getVehicleDetails = [];  
     this.service.getVehicle().map(res => res).subscribe(data => {
       var SubString = data.match(/\[(.*?)\]/);
       this.getVehicleDetails.push(SubString[0])
       this.getVehicleDetails = JSON.parse(this.getVehicleDetails[0]);
       this.getVehicleDetails.forEach(data =>{ 
+        
         if (this.vehicleNumber.indexOf(data["RegNo"]) == -1) {
           this.vehicleNumber.push(data["RegNo"])
         }     
       });
+      this.options = this.vehicleNumber;    
+      this.filteredOptions = this.myControl.valueChanges
+        .pipe(
+          startWith(''),
+          map(value => this._filter(value))
+        );        
     });
   }
-
-
-
 }
